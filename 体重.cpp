c1327f45b732a81d9bc8b9fb89e3bfe721425d4f
wnz98221-dotcom <wnz98221@gmail.com>
2026-09-10 21:31:13 +0800

    Add files via upload

---
+++ 体重.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+main(void)
+{float weight,height;
+cout<<"put your motherfucking height and weight in this page"<<endl;
+cin>>weight>>height;
+float BMI;
+BMI=weight/height*height;
+
+cout<<"BMI"<<endl;
+if(BMI<25)
+{cout<<"your BMI is in a probable range"<<endl;
+}
+else
+{if(BMI>=28){cout<<"dick"<<endl;
+}
+else{cout<<"fuck "<<endl;
+
+}
+}
+return 0;
+}
