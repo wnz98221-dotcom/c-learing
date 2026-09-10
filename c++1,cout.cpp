26882048f3982141e6d9d7cf2f54a243a984ef6b
wnz98221-dotcom <wnz98221@gmail.com>
2026-09-10 19:20:25 +0800

    Add files via upload

---
+++ c++1,cout.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main (void)
+{int var=3;
+var +=3;
+cout<<"var="<<var<<endl;
+
+return 0;
+}
---
+++ varies change.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(void)
+
+{ 
+int var1=12;
+float var2=1.2;
+float var3;    
+ var3 = var1/var2;
+cout<<"var3="<<var3;
+
+return 0;
+}
